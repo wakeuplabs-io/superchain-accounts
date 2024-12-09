f8636f8b1a512864a331bedb232089292009ab1d
Matias Barro <matias.barro@gmail.com>
2024-12-09 17:59:48 -0300

    Add bundler deployment config to sst.config

--- packages/bundler/sst.config.ts
+++ packages/bundler/sst.config.ts
@@ -1,5 +1,8 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+const BUNDLER_PORT = Number(process.env.PORT ?? 4337);
+const BUNDLER_PORT_FORWARDING = `${BUNDLER_PORT}/http` as `${number}/http`;
+
 export default $config({
   app(input) {
     return {
@@ -14,6 +17,7 @@ export default $config({
             tags: {
               customer: "optimism-gov",
               application: "superchain",
+              environment: input?.stage,
             }
           },
         },
@@ -27,15 +31,38 @@ export default $config({
     cluster.addService("bundler-service", {
       loadBalancer: {
         ports: [
-          { listen: "80/http", forward: "4000/http" },
-        ]
+          { listen: "80/http", forward: BUNDLER_PORT_FORWARDING},
+        ],
+        health: {
+          [BUNDLER_PORT_FORWARDING]: {
+            path: "/health",
+            interval: "10 seconds",
+          }
+        }
       },
-      environment: {
-        PORT: "4000",
+      image: {
+        dockerfile: "Dockerfile",
+        args: {
+          BUNDLER_VERSION: process.env.BUNDLER_VERSION ?? "v1.2.2"
+        }
       },
-      dev: {
-        command: "node --watch index.mjs",
+      health: {
+        command: ["CMD-SHELL", `curl -f http://localhost:${BUNDLER_PORT}/health || exit 1`],
+        startPeriod: "60 seconds",
+        timeout: "5 seconds",
+        interval: "30 seconds",
+        retries: 3,
       },
+      command: [
+        "--entrypoints", process.env.ENTRYPOINTS,
+        "--executor-private-keys",process.env.EXECUTOR_PRIVATE_KEYS,
+        "--utility-private-key", process.env.UTILITY_PRIVATE_KEY, 
+        "--rpc-url", process.env.RPC_URL,
+        "--port", BUNDLER_PORT.toString(),
+        "--deploy-simulations-contract", "true",
+        "--chain-type","op-stack",
+        "--safe-mode", "false"
+      ],
     });
   }
 });
