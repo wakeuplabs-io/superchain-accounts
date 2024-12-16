1bfd22712bb6b7c0e783436d7f679ca1b5e48806
Matias Barro <matias.barro@gmail.com>
2024-12-16 18:48:51 -0300

    Add authenticated side bar button

---
+++ packages/ui/src/components/_authenticated/sidebar/authenticated-sidebar-menu-button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { LucideIcon } from "lucide-react";
+import { SidebarMenuButton } from "@/components/ui/sidebar";
+
+type AuthenticatedSidebarMenuButtonProps = {
+    Icon: LucideIcon
+    text: string
+    route: string
+};
+
+export const AuthenticatedSidebarMenuButton: React.FC<AuthenticatedSidebarMenuButtonProps> = ({ Icon, text, route }) => {
+  return (
+    <SidebarMenuButton size='lg' asChild>
+      <a href={route} className="font-medium text-custom-slate-50">
+        <Icon className="h-4 w-4" strokeWidth={3} />
+        <span className="text-base">{text}</span>
+      </a>
+    </SidebarMenuButton>
+  );
+};
---
+++ packages/ui/src/components/_authenticated/sidebar/index.ts
@@ -0,0 +1,1 @@
+export * from "./authenticated-sidebar-menu-button";
