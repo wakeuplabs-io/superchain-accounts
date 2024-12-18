1d489a6add28a72075495298a7471bff494976ec
Matias Barro <matias.barro@gmail.com>
2024-12-18 11:21:45 -0300

    Get screen size breakpoint from tailwind in use-mobile

--- packages/ui/src/hooks/use-mobile.tsx
+++ packages/ui/src/hooks/use-mobile.tsx
@@ -1,19 +1,24 @@
-import * as React from "react"
+import * as React from "react";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../tailwind.config";
 
-const MOBILE_BREAKPOINT = 768
+const fullConfig = resolveConfig(tailwindConfig);
+
+const MOBILE_BREAKPOINT = Number(fullConfig.theme.screens.md.replace("px", ""));
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    console.log("mobile breakpoint", MOBILE_BREAKPOINT);
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+    mql.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
 
-  return !!isMobile
+  return !!isMobile;
 }
