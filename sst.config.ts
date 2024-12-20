72e90545f9c3f494a89dc2fc6c64e80ce6ac00ec
Matias Barro <matias.barro@gmail.com>
2024-12-20 16:19:55 -0300

    Add sst config

---
+++ sst.config.ts
@@ -0,0 +1,33 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "superchain-app",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+      providers: {
+        aws: {
+          region: process.env.AWS_REGION,
+          profile: process.env.AWS_PROFILE,
+          defaultTags: {
+            tags: {
+              customer: "optimism-gov",
+              application: "superchain-app",
+              environment: input?.stage,
+            }
+          },
+        },
+      }
+    };
+  },
+  async run() {
+    // Create site
+    new sst.aws.StaticSite("frontend", {
+      build: {
+        command: "pnpm --filter ./packages/ui run build",
+        output: "packages/ui/dist",
+      },
+    });
+  }
+});
