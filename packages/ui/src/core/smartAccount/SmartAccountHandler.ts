5958b74a37b06359bd679aa26e5ed3b013046838
Matias Barro <matias.barro@gmail.com>
2024-12-26 16:47:53 -0300

    Add logic for smart account initialization

--- packages/ui/src/core/smartAccount/SmartAccountHandler.ts
+++ packages/ui/src/core/smartAccount/SmartAccountHandler.ts
@@ -11,24 +11,23 @@ import {
 
 
 export class SmartAccountHandler {
-  private readonly bundlerClient: BundlerClient;
+  private bundlerClient: BundlerClient | null = null;
+  private smartAccount: SmartAccount | null = null;
 
   constructor(
     private readonly publicClient: PublicClient,
     private readonly entryPointAddress: Address,
     private readonly smartAccountFactoryAddress: Address,
-    bundlerUrl: string,
-    paymaster: PaymasterClient,
-  ) {
-    this.bundlerClient = createBundlerClient({
-      chain: this.publicClient.chain,
-      transport: http(bundlerUrl),
-      paymaster,
-    });
+    private readonly bundlerUrl: string,
+    private readonly paymaster: PaymasterClient,
+  ) {}
+
+  getSmartAccount(): SmartAccount | null {
+    return this.smartAccount;
   }
 
-  async getSmartAccount(owner: ToSimpleSmartAccountParameters<EntryPointVersion>["owner"]): Promise<SmartAccount> {
-    return toSimpleSmartAccount({
+  async initialize(owner: ToSimpleSmartAccountParameters<EntryPointVersion>["owner"]): Promise<SmartAccount> {
+    this.smartAccount = await toSimpleSmartAccount({
       owner,
       client: this.publicClient,
       factoryAddress: this.smartAccountFactoryAddress,
@@ -37,20 +36,46 @@ export class SmartAccountHandler {
         version: "0.7",
       },
     });
+    
+    //initialize bundler client
+    this.bundlerClient = createBundlerClient({
+      account: this.smartAccount,
+      chain: this.publicClient.chain,
+      transport: http(this.bundlerUrl),
+      paymaster: this.paymaster,
+    });
+
+    const isDeployed = await this.smartAccount.isDeployed();
+
+    if(!isDeployed) {
+      console.log("Deploying smart account", this.smartAccount.address);
+      // Send a user operation to the bundler to deploy the smart account
+      const transaction = {
+        to: this.smartAccount.address,
+        value: BigInt(0),
+      };
+      
+      await this.sendTransaction(transaction);
+    }
+
+    return this.smartAccount;
   }
 
-  async sendTransaction(account: SmartAccount, transaction: UserOperationCall) {
+  async sendTransaction(transaction: UserOperationCall) {
+    if(!this.bundlerClient) {
+      throw Error("Bundler client not initialized");
+    }
+
     //TODO: INVESTIGATE THOSE GAS MAGIC NUMBERS
     const hash = await this.bundlerClient.sendUserOperation({
-      account,
       calls: [transaction],
       maxPriorityFeePerGas: 1000304n,
       maxFeePerGas: 1000304n,
       paymasterVerificationGasLimit: 1000304n,
     });
 
-    const receipt = await this.bundlerClient.getUserOperationReceipt({ hash });
-
+    const receipt = await this.bundlerClient.waitForUserOperationReceipt({ hash });
+    
     console.log("Transaction receipt:", receipt);
   }
 }
--- packages/ui/src/routes/_authenticated/index.tsx
+++ packages/ui/src/routes/_authenticated/index.tsx
@@ -11,14 +11,24 @@ function Index() {
   const smartAccountHandler  = useSuperChainStore(state => state.smartAccountHandler);
   const authHandler = useSuperChainStore(state => state.authHandler);
   const [smartAccount, setSmartAccount] = useState<SmartAccount>();
+  const [isInitializing, setIsInitializing] = useState(false);
 
   useEffect(() => {
-    smartAccountHandler.getSmartAccount(authHandler.getProvider()).then(setSmartAccount);
+    setIsInitializing(true);
+    smartAccountHandler.initialize(authHandler.getProvider()).then(setSmartAccount).finally(() => setIsInitializing(false));
   }, []);
+
+  useEffect(() => {
+    if(!smartAccount) {
+      return;
+    }
+    smartAccount?.isDeployed().then(result => console.log("isDeployed", result));
+  }, [smartAccount]);
 
   return (
     <div className="flex flex-1 flex-col  items-center justify-center">
       <h2>Home Page</h2>
+      {isInitializing && <h3>Initializing smart account, please confirm the transaction in your wallet...</h3>}
       {smartAccount && <h3>Smart Account: {smartAccount.address.toString()}</h3>}
     </div>
   );
