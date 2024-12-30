d294f6b1a1eb6750c0d0594aac421c3efb80637a
Matias Barro <matias.barro@gmail.com>
2024-12-30 18:10:30 -0300

    Improve user experience

--- packages/ui/src/routes/_authenticated/index.tsx
+++ packages/ui/src/routes/_authenticated/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import { SmartAccount } from "viem/account-abstraction";
 import { useSuperChainStore } from "@/core/store";
+import { Button } from "@/components";
 
 export const Route = createFileRoute("/_authenticated/")({
   component: Index,
@@ -12,8 +13,10 @@ function Index() {
   const authHandler = useSuperChainStore(state => state.authHandler);
   const web3Client = useSuperChainStore(state => state.web3Client);
   const [smartAccount, setSmartAccount] = useState<{ account: SmartAccount | null, isDeployed: boolean }>({account: null, isDeployed: false});
-  const [isInitializing, setIsInitializing] = useState(false);
   const [accountBalance, setAccountBalance] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [isInitializing, setIsInitializing] = useState(false);
+  const [isDeploying, setIsDeploying] = useState(false);
 
   useEffect(() => {
     setIsInitializing(true);
@@ -37,6 +40,21 @@ function Index() {
 
     getSmartAccountBalance(smartAccount.account).then(setAccountBalance);
   }, [smartAccount?.account, smartAccount?.isDeployed, web3Client]);
+
+  const deploySmartAccount = async () => {
+    setIsDeploying(true);
+    try {
+      await smartAccountHandler.deploySmartAccount();
+      setSmartAccount(prev => ({...prev, isDeployed: true}));
+    } catch(error) {
+      if(error instanceof Error) {
+        setError(error.message);
+      }
+      setError("Error deploying smart account");
+    } finally {
+      setIsDeploying(false);
+    }
+  };
   
   //TODO: IMPROVE THIS CODE
   if(isInitializing) {
@@ -61,7 +79,14 @@ function Index() {
     <div className="flex flex-col gap-3">
       <p className="text-3xl font-bold">Accounts</p>
         <p><b>Smart Account Address:</b> {smartAccount.account.address.toString()}</p>
-
+        {smartAccount.isDeployed && <p><b>Account Balance:</b> {accountBalance} ETH</p>}
+        {!smartAccount.isDeployed && (
+          <div className="flex flex-row gap-2 items-center">
+            <span>Smart account not deployed, click the next button to deploy it and confirm the transaction in your wallet.</span>
+            <Button size='sm' onClick={deploySmartAccount} loading={isDeploying}>Deploy Account</Button>
+          </div>
+        )}
+        {error && <div className="text-xl text-red-500">Error: {error}</div>}
     </div>
   );
 }
