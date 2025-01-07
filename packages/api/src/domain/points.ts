9be6b373ac1a96a7cb8fa7e8d104598bd1bcbf3e
Luciano Pérez Cerra <perezcerraluciano@gmail.com>
2025-01-07 11:58:52 -0300

    restore unused changes made for dev

--- packages/api/src/domain/points.ts
+++
@@ -1,9 +0,0 @@
-import { EventDef, NormalizedCryptoEvent } from "@/types/index.js";
-import e from "cors";
-
-export class PointsService {
-  constructor() {}
-  calculatePoints = (event: EventDef): number => {
-    return event.points_awarded;
-  };
-}
