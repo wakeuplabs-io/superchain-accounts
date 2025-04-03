f412521295990ad2adee7c5664e672ed35baaeb5
Matias Zapata <matuzapata@gmail.com>
2025-04-03 15:48:38 -0300

    cleanup

--- packages/contracts/contracts/SuperchainPoints.sol
+++ packages/contracts/contracts/SuperchainPoints.sol
@@ -7,13 +7,12 @@ import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {PredeployAddresses} from "./libraries/SuperchainPredeployAddresses.sol";
 
-/// @title SuperchainERC20
+/// @title SuperchainPoints
 /// @notice A standard ERC20 extension implementing IERC7802 for unified cross-chain fungibility across
 ///         the Superchain. Allows the SuperchainTokenBridge to mint and burn tokens as needed.
 contract SuperchainPoints is ERC20, IERC7802, Ownable {
-    string private _name;
-    string private _symbol;
-    uint8 private immutable _decimals;
+    /// @dev Unauthorized error
+    error Unauthorized();
 
     constructor(
         address owner,
@@ -21,18 +20,32 @@ contract SuperchainPoints is ERC20, IERC7802, Ownable {
         string memory symbol
     ) ERC20(name, symbol) Ownable(owner) {}
 
+    /// @notice Allows owner to mint tokens
+    /// @param to address that receives the tokens
+    /// @param amount token amount
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+    }
+
+    /// @notice Allows owner to mint a batch of tokens
+    /// @param to array of addresses that receives the tokens
+    /// @param amount array of token amounts
+    function batchMint(
+        address[] memory to,
+        uint256[] memory amount
+    ) external onlyOwner {
+        for (uint256 i = 0; i < to.length; i++) {
+            _mint(to[i], amount[i]);
+        }
     }
 
     /// @notice Allows the SuperchainTokenBridge to mint tokens.
     /// @param _to     Address to mint tokens to.
     /// @param _amount Amount of tokens to mint.
     function crosschainMint(address _to, uint256 _amount) external {
-        require(
-            msg.sender == PredeployAddresses.SUPERCHAIN_TOKEN_BRIDGE,
-            "Unauthorized"
-        );
+        if (msg.sender == PredeployAddresses.SUPERCHAIN_TOKEN_BRIDGE) {
+            revert Unauthorized();
+        }
 
         _mint(_to, _amount);
 
@@ -43,11 +56,10 @@ contract SuperchainPoints is ERC20, IERC7802, Ownable {
     /// @param _from   Address to burn tokens from.
     /// @param _amount Amount of tokens to burn.
     function crosschainBurn(address _from, uint256 _amount) external {
-        require(
-            msg.sender == PredeployAddresses.SUPERCHAIN_TOKEN_BRIDGE,
-            "Unauthorized"
-        );
-
+        if (msg.sender == PredeployAddresses.SUPERCHAIN_TOKEN_BRIDGE) {
+            revert Unauthorized();
+        }
+        
         _burn(_from, _amount);
 
         emit CrosschainBurn(_from, _amount, msg.sender);
