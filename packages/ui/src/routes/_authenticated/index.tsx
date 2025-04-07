3a1981dfacf9404adb34255f0eb524b123d84491
Matias Barro <matias.barro@gmail.com>
2025-04-07 12:53:55 -0300

    Simplify accounts page

--- packages/ui/src/routes/_authenticated/index.tsx
+++ packages/ui/src/routes/_authenticated/index.tsx
@@ -1,7 +1,5 @@
-import { Button } from "@/components";
 import { useSuperChainAccount } from "@/context/SmartAccountContext";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
 import { formatEther } from "viem";
 
 export const Route = createFileRoute("/_authenticated/")({
@@ -9,15 +7,7 @@ export const Route = createFileRoute("/_authenticated/")({
 });
 
 function Index() {
-  const { account, deploySmartAccount } = useSuperChainAccount();
-  const [isDeploying, setIsDeploying] = useState(false);
- 
-  const deployAccount = async () => {
-    setIsDeploying(true);
-    await deploySmartAccount();
-    setIsDeploying(false);
-  };
-
+  const { account } = useSuperChainAccount();
   
   // //TODO: IMPROVE THIS CODE
   if(account.status === "pending") {
@@ -33,12 +23,7 @@ function Index() {
     <div className="flex flex-col gap-3">
       <p className="text-3xl font-bold">Accounts</p>
       <p><b>Smart Account Address:</b> {account.instance.address.toString()}</p>
-      {account.status === "deployed" ? <p><b>Account Balance:</b> {formatEther(account.balance)} ETH</p> : (
-        <div className="flex flex-row gap-2 items-center">
-          <span>Smart account not deployed, click the next button to deploy it and confirm the transaction in your wallet.</span>
-          <Button size='sm' onClick={deployAccount} loading={isDeploying}>Deploy Account</Button>
-        </div>
-      )}
+      <p><b>Account Balance:</b> {formatEther(account.balance)} ETH</p>
     </div>
   );
   
