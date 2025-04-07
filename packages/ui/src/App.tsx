d71c95b4d375cced1318869891b1d7e6908047a2
Matias Barro <matias.barro@gmail.com>
2025-04-07 14:51:45 -0300

    Replace InnerApp component with a RouterProvider HOC

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,19 +1,13 @@
-import { router } from "@/shared/config/tanstackRouter";
-import { RouterProvider } from "@tanstack/react-router";
 import { Web3Provider } from "./context/Web3Context";
-import { AuthProvider, useAuth } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
+import { RouterProvider } from "./hoc/RouterProvider";
 
-function InnerApp() {
-  return <RouterProvider router={router} context={{
-    auth: useAuth()
-  }} />;
-}
 
 function App() {
   return (
     <Web3Provider>
       <AuthProvider>
-        <InnerApp />
+        <RouterProvider />
       </AuthProvider>
     </Web3Provider>
   );
