a57fdce0172a977869e7573309e398a5af8f4fb6
Matias Barro <matias.barro@gmail.com>
2025-04-08 18:20:10 -0300

    Add receive tokens feature

--- packages/ui/src/components/_authenticated/smart-account-card/action-button.tsx
+++ packages/ui/src/components/_authenticated/smart-account-card/action-button.tsx
@@ -5,15 +5,17 @@ import { ButtonHTMLAttributes } from "react";
 interface ActionButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     icon: LucideIcon
     text: string
+    isDisabled?: boolean
     onClick: () => void
     className?: string
 }
 
-export const ActionButton = ({ text, icon: Icon, className, onClick }: ActionButtonProps) => {
+export const ActionButton = ({ text, icon: Icon, className, isDisabled, onClick }: ActionButtonProps) => {
   return (
     <button
+      disabled={isDisabled}
       onClick={onClick}
-      className={clsx("flex items-center justify-center gap-2 rounded-xl text-custom-slate-300 border border-custom-gray-200 hover:bg-gray-50 py-3 px-7", className)} >
+      className={clsx("flex items-center justify-center gap-2 rounded-xl text-custom-slate-300 border border-custom-gray-200 hover:bg-gray-50 py-3 px-7 disabled:bg-gray-100", className)} >
       <Icon className="w-5 h-5" />
       <span className="text-base">{text}</span>
     </button>
--- packages/ui/src/components/_authenticated/smart-account-card/smart-account-card.tsx
+++ packages/ui/src/components/_authenticated/smart-account-card/smart-account-card.tsx
@@ -1,21 +1,37 @@
 import { useWeb3 } from "@/context/Web3Context";
 import { ActionButton } from "./action-button";
 import { Download } from "lucide-react";
+import { useState } from "react";
+import { ReceiveTokensDialog } from "@/components/_authenticated/smart-account-card/receive-tokens-dialog";
+import { useSuperChainAccount } from "@/context/SmartAccountContext";
 
 export function SmartAccountCard(){
   const { chain } = useWeb3();
+  const { account } = useSuperChainAccount();
+  const [ isReceiveDialogOpen, setIsReceiveDialogOpen ] = useState(false);
+
+  if(account.status === "pending") return null;
+
   return (
-    <div className="flex flex-col rounded-lg shadow-sm bg-white p-8 max-w-screen-xl gap-8">
-      <div className="flex flex-row gap-4 items-center">
-        <img className="w-16 h-16" src={chain.logo} />
-        <div className="flex flex-col gap-1">
-          <h2 className="text-3xl text-black font-semibold">{chain.data.name}</h2>
-          <span className="text-base text-black font-normal">Smart Account</span>
+    <>
+      <div className="flex flex-col rounded-lg shadow-sm bg-white p-8 max-w-screen-xl gap-8">
+        <div className="flex flex-row gap-4 items-center">
+          <img className="w-16 h-16" src={chain.logo} />
+          <div className="flex flex-col gap-1">
+            <h2 className="text-3xl text-black font-semibold">{chain.data.name}</h2>
+            <span className="text-base text-black font-normal">Smart Account</span>
+          </div>
+        </div>
+        <div className="flex items-start w-full">
+          <ActionButton text='Receive' icon={Download} onClick={() => setIsReceiveDialogOpen(true)} />
         </div>
       </div>
-      <div className="flex items-start w-full">
-        <ActionButton text='Receive' icon={Download} onClick={()=>console.log("asdd")} />
-      </div>
-    </div>
+      <ReceiveTokensDialog
+        isOpen={isReceiveDialogOpen}
+        onClose={()=>setIsReceiveDialogOpen(false)}
+        address={account.instance?.address}
+        chain={chain}
+      />
+    </>
   );
 }
