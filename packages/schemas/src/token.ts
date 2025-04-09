cf50daa8085696b3023285f1e9411c60c076e903
Matias Barro <matias.barro@gmail.com>
2025-04-09 18:05:13 -0300

    Update token schemas

--- packages/schemas/src/token.ts
+++ packages/schemas/src/token.ts
@@ -1,22 +1,29 @@
 import { isAddress } from "viem";
 import { z } from "zod";
 
-export const addTokenRequestSchema = z.object({
+export const importUserTokenRequestSchema = z.object({
+  chainId: z.number({required_error: "Chain is required"}),
+  userAddress:  z
+    .string({
+      required_error: "User address is required",
+    })
+    .refine((val) => isAddress(val), "Invalid user address"),
   address: z
     .string({
       required_error: "Contract address is required",
-      invalid_type_error: "Invalid contract address",
     })
     .refine((val) => isAddress(val), "Invalid contract address"),
 });
 
-export type AddTokenRequest = z.infer<typeof addTokenRequestSchema>
+export type ImportUserTokenRequest = z.infer<typeof importUserTokenRequestSchema>
 
-const tokenSchema = z.object({
+const userTokenSchema = z.object({
   address: z.string().refine((val) => isAddress(val)),
+  userAddress: z.string().refine((val) => isAddress(val)),
+  chainId: z.number(),
   name: z.string(),
   symbol: z.string(),
   logo: z.string().optional(),
 });
 
-export type Token = z.infer<typeof tokenSchema>;
+export type UserToken = z.infer<typeof userTokenSchema>;
