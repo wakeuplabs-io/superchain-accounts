0e01301d01ebe95c5b40c26e98206d5ae80bd46a
Matias Barro <matias.barro@gmail.com>
2025-04-14 18:53:00 -0300

    First version of amount field

---
+++ packages/ui/src/components/home/send-token-dialog/amount-field.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import BigNumber from "bignumber.js";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Asset, useAssets } from "@/hooks/use-assets";
+import { useFormContext } from "react-hook-form";
+import { SendTokenSchema } from "./send-token-dialog";
+import { Input } from "@/components/ui/input";
+
+function computeAmount(amount: string, asset: Asset): bigint {
+  const bn = new BigNumber(amount || "0").multipliedBy(
+    new BigNumber(10).pow(asset.decimals)
+  );
+  if (!bn.isInteger()) return 0n; // should be fixed point
+  return BigInt(bn.toFixed(0));
+}
+
+const AmountField = () => {
+  const { control, setValue, watch} = useFormContext<SendTokenSchema>();
+  const {isPending, error, data} = useAssets();
+  const [controlledValue, setControlledValue] = useState<string>("0");
+  const [currentAsset, setCurrentAsset] = useState<Asset | null>(null);
+  
+  const selectedAsset = watch("asset");
+
+  useEffect(() => {
+    if(isPending || error) return;
+
+    const asset = data.find((asset) => asset.symbol === selectedAsset);
+    
+    if(!asset) {
+      throw new Error("Asset not found"); 
+    }
+
+    setCurrentAsset(asset);
+    setControlledValue(new BigNumber(controlledValue).toFixed(asset.decimals));
+    const amount = computeAmount(controlledValue, asset);
+    setValue("amount", amount);
+  }, [selectedAsset]);
+
+  return  (
+    <FormField
+      control={control}
+      name="amount"
+      render={({field}) => {
+        return (
+          <FormItem>
+            <FormLabel>Amount</FormLabel>
+            <FormControl>
+              <Input {...field} className="h-14" type="number" value={controlledValue} onChange={(e: React.ChangeEvent<HTMLInputElement>)=> {
+                const val = e.target.value;
+
+                if(!currentAsset) return;
+
+                // Validate decimal places
+                const [_, fraction] = val.split(".");
+
+                if (fraction && fraction.length > currentAsset.decimals) return; // Too many decimals
+
+                setControlledValue(val);
+
+                const newAmount = computeAmount(val, currentAsset);
+                field.onChange(newAmount);
+              }} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        );}}
+    />
+  );
+};
+
+export default AmountField;
--- packages/ui/src/components/home/send-token-dialog/asset-selector.tsx
+++ packages/ui/src/components/home/send-token-dialog/asset-selector.tsx
@@ -18,51 +18,48 @@ const AssetSelector = () => {
   const defaultAsset = useMemo(() => {
     return data?.find((asset) => !!asset.native) ?? data[0];
   }, [data]);
-
   
   return (
-    <>
-      <FormField
-        control={control}
-        name="asset"
-        defaultValue={defaultAsset?.symbol}
-        render={({field}) => {
-          return (
-            <FormItem>
-              <FormLabel>Asset</FormLabel>
-              <FormControl>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <SelectTrigger className="h-14 focus:ring-0 ">
-                    <SelectValue placeholder="Select a token" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {data?.map((asset) => {
-                      return (
-                        <SelectItem key={asset.symbol} value={asset.symbol}>
-                          <div className="flex items-center gap-x-2">
-                            <div className="rounded-full bg-gray-100 flex items-center justify-center">
-                              <img src={asset.logoURI ?? defaultTokenLogo} alt={asset.name} className="w-8 h-8" />
-                            </div>
-                            <div className="flex flex-col gap-0 items-start">
-                              <span className="text-base font-normal text-black">{asset.symbol}</span>
-                              <span className="text-xs font-normal text-gray-500">{formatUnits(asset.balance, asset.decimals)}</span>
-                            </div>
+    <FormField
+      control={control}
+      name="asset"
+      defaultValue={defaultAsset?.symbol}
+      render={({field}) => {
+        return (
+          <FormItem>
+            <FormLabel>Asset</FormLabel>
+            <FormControl>
+              <Select
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+              >
+                <SelectTrigger className="h-14 focus:ring-0 ">
+                  <SelectValue placeholder="Select a token" />
+                </SelectTrigger>
+                <SelectContent>
+                  {data?.map((asset) => {
+                    return (
+                      <SelectItem key={asset.symbol} value={asset.symbol}>
+                        <div className="flex items-center gap-x-2">
+                          <div className="rounded-full bg-gray-100 flex items-center justify-center">
+                            <img src={asset.logoURI ?? defaultTokenLogo} alt={asset.name} className="w-8 h-8" />
                           </div>
-                        </SelectItem>
-                      );  
-                    })}
-                  </SelectContent>
-                </Select>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          );
-        }}
-      />
-    </>
+                          <div className="flex flex-col gap-0 items-start">
+                            <span className="text-base font-normal text-black">{asset.symbol}</span>
+                            <span className="text-xs font-normal text-gray-500">{formatUnits(asset.balance, asset.decimals)}</span>
+                          </div>
+                        </div>
+                      </SelectItem>
+                    );  
+                  })}
+                </SelectContent>
+              </Select>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />
   );
 };
 
