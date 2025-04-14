bfa8568334a3e1cc4f70cf44ddb7b6c00836b70d
Matias Zapata <matuzapata@gmail.com>
2025-04-14 18:37:49 -0300

    setup badges deployment with uri

--- packages/contracts/ignition/modules/SuperchainBadges.ts
+++ packages/contracts/ignition/modules/SuperchainBadges.ts
@@ -2,8 +2,20 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const SuperchainBadgesModule = buildModule("SuperchainBadgesModule", (m) => {
   const owner = m.getParameter<string>("Owner");
+  const baseUri = m.getParameter<string>("BaseURI");
 
   const SuperchainBadges = m.contract("SuperchainBadges", [owner]);
+
+  m.call(SuperchainBadges, "setURI", [1, baseUri + "-1.json"]);
+  m.call(SuperchainBadges, "setURI", [2, baseUri + "-2.json"]);
+  m.call(SuperchainBadges, "setURI", [3, baseUri + "-3.json"]);
+  m.call(SuperchainBadges, "setURI", [4, baseUri + "-4.json"]);
+  m.call(SuperchainBadges, "setURI", [5, baseUri + "-5.json"]);
+  m.call(SuperchainBadges, "setURI", [6, baseUri + "-6.json"]);
+  m.call(SuperchainBadges, "setURI", [7, baseUri + "-7.json"]);
+  m.call(SuperchainBadges, "setURI", [8, baseUri + "-8.json"]);
+  m.call(SuperchainBadges, "setURI", [9, baseUri + "-9.json"]);
+
 
   return { SuperchainBadges };
 });
