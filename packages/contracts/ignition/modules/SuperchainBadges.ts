7a0a39952106119c477dcf0cf1d0f1ae2df40817
Matias Zapata <matuzapata@gmail.com>
2025-04-14 19:27:33 -0300

    fix script

--- packages/contracts/ignition/modules/SuperchainBadges.ts
+++ packages/contracts/ignition/modules/SuperchainBadges.ts
@@ -6,15 +6,15 @@ const SuperchainBadgesModule = buildModule("SuperchainBadgesModule", (m) => {
 
   const SuperchainBadges = m.contract("SuperchainBadges", [owner]);
 
-  m.call(SuperchainBadges, "setURI", [1, baseUri + "-1.json"], { id: "setURI_1" });
-  m.call(SuperchainBadges, "setURI", [2, baseUri + "-2.json"], { id: "setURI_2" });
-  m.call(SuperchainBadges, "setURI", [3, baseUri + "-3.json"], { id: "setURI_3" });
-  m.call(SuperchainBadges, "setURI", [4, baseUri + "-4.json"], { id: "setURI_4" });
-  m.call(SuperchainBadges, "setURI", [5, baseUri + "-5.json"], { id: "setURI_5" });
-  m.call(SuperchainBadges, "setURI", [6, baseUri + "-6.json"], { id: "setURI_6" });
-  m.call(SuperchainBadges, "setURI", [7, baseUri + "-7.json"], { id: "setURI_7" });
-  m.call(SuperchainBadges, "setURI", [8, baseUri + "-8.json"], { id: "setURI_8" });
-  m.call(SuperchainBadges, "setURI", [9, baseUri + "-9.json"], { id: "setURI_9" });
+  m.call(SuperchainBadges, "setURI", [1, `${baseUri}-1.json`], { id: "setURI_1" });
+  m.call(SuperchainBadges, "setURI", [2, `${baseUri}-2.json`], { id: "setURI_2" });
+  m.call(SuperchainBadges, "setURI", [3, `${baseUri}-3.json`], { id: "setURI_3" });
+  m.call(SuperchainBadges, "setURI", [4, `${baseUri}-4.json`], { id: "setURI_4" });
+  m.call(SuperchainBadges, "setURI", [5, `${baseUri}-5.json`], { id: "setURI_5" });
+  m.call(SuperchainBadges, "setURI", [6, `${baseUri}-6.json`], { id: "setURI_6" });
+  m.call(SuperchainBadges, "setURI", [7, `${baseUri}-7.json`], { id: "setURI_7" });
+  m.call(SuperchainBadges, "setURI", [8, `${baseUri}-8.json`], { id: "setURI_8" });
+  m.call(SuperchainBadges, "setURI", [9, `${baseUri}-9.json`], { id: "setURI_9" });
 
 
   return { SuperchainBadges };
