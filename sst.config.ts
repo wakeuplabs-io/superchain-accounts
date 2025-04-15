05772e6863537fddf42dea40bfd696f310e3325c
MartinC <59508264+martinc1991@users.noreply.github.com>
2025-04-15 07:04:31 -0300

    Add sst AWS default tags

--- sst.config.ts
+++ sst.config.ts
@@ -1,19 +1,21 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-const PROJECT_NAME = "superchain";
+const CUSTOMER = 'optimism';
+const PROJECT_NAME = 'superchain';
 
 export default $config({
   app(input) {
     return {
       name: PROJECT_NAME,
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws',
       providers: {
         aws: {
           defaultTags: {
             tags: {
-              customer: "optimism",
-              application: "superchain",
+              customer: CUSTOMER,
+              application: PROJECT_NAME,
+              project: PROJECT_NAME,
               environment: input?.stage,
             },
           },
@@ -23,17 +25,17 @@ export default $config({
   },
   async run() {
     // load environment variables
-    const dotenv = await import("dotenv");
+    const dotenv = await import('dotenv');
     dotenv.config();
 
     // deploy api
     const api = new sst.aws.Function(`${PROJECT_NAME}-api`, {
-      handler: "packages/api/src/app.handler",
+      handler: 'packages/api/src/app.handler',
       url: {
         cors: false,
       },
       environment: {
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
         DATABASE_URL: process.env.DATABASE_URL!,
         BUNDLER_UNICHAIN_SEPOLIA: process.env.BUNDLER_UNICHAIN_SEPOLIA!,
         BUNDLER_OPTIMISM_SEPOLIA: process.env.BUNDLER_OPTIMISM_SEPOLIA!,
@@ -49,17 +51,16 @@ export default $config({
         SUPERCHAIN_POINTS_ADDRESS: process.env.SUPERCHAIN_POINTS_ADDRESS!,
         CRONJOB_KEY: process.env.CRONJOB_KEY!,
         MULTICALL_CONTRACT_ADDRESS: process.env.MULTICALL_CONTRACT_ADDRESS!,
-        PRISMA_QUERY_ENGINE_LIBRARY:
-          "/var/task/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node",
+        PRISMA_QUERY_ENGINE_LIBRARY: '/var/task/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node',
       },
       copyFiles: [
         {
-          from: "node_modules/.prisma/client/",
-          to: ".prisma/client/",
+          from: 'node_modules/.prisma/client/',
+          to: '.prisma/client/',
         },
         {
-          from: "node_modules/@prisma/client/",
-          to: "@prisma/client/",
+          from: 'node_modules/@prisma/client/',
+          to: '@prisma/client/',
         },
       ],
     });
@@ -67,20 +68,20 @@ export default $config({
     // deploy cron job
     const cron = new sst.aws.Cron(`${PROJECT_NAME}-cron`, {
       function: {
-        handler: "packages/api/src/cron.handler",
+        handler: 'packages/api/src/cron.handler',
         environment: {
           API_URL: api.url,
           CRONJOB_KEY: process.env.CRONJOB_KEY!,
         },
       },
-      schedule: "cron(0 12 * * ? *)",
+      schedule: 'cron(0 12 * * ? *)',
     });
 
     // deploy ui
     const ui = new sst.aws.StaticSite(`${PROJECT_NAME}-ui`, {
       build: {
-        command: "npm run build --workspace=ui",
-        output: "packages/ui/dist",
+        command: 'npm run build --workspace=ui',
+        output: 'packages/ui/dist',
       },
       domain: `${PROJECT_NAME}.wakeuplabs.link`,
       environment: {
@@ -91,18 +92,15 @@ export default $config({
         VITE_RPC_BASE_SEPOLIA: process.env.RPC_BASE_SEPOLIA!,
         VITE_RPC_OPTIMISM_SEPOLIA: process.env.RPC_OPTIMISM_SEPOLIA!,
         VITE_RPC_UNICHAIN_SEPOLIA: process.env.RPC_UNICHAIN_SEPOLIA!,
-        VITE_ENTRYPOINT_UNICHAIN_SEPOLIA:
-          process.env.ENTRYPOINT_UNICHAIN_SEPOLIA!,
-        VITE_ENTRYPOINT_OPTIMISM_SEPOLIA:
-          process.env.ENTRYPOINT_OPTIMISM_SEPOLIA!,
+        VITE_ENTRYPOINT_UNICHAIN_SEPOLIA: process.env.ENTRYPOINT_UNICHAIN_SEPOLIA!,
+        VITE_ENTRYPOINT_OPTIMISM_SEPOLIA: process.env.ENTRYPOINT_OPTIMISM_SEPOLIA!,
         VITE_ENTRYPOINT_BASE_SEPOLIA: process.env.ENTRYPOINT_BASE_SEPOLIA!,
         VITE_EXPLORER_BASE_SEPOLIA: process.env.EXPLORER_BASE_SEPOLIA!,
         VITE_EXPLORER_OPTIMISM_SEPOLIA: process.env.EXPLORER_OPTIMISM_SEPOLIA!,
         VITE_EXPLORER_UNICHAIN_SEPOLIA: process.env.EXPLORER_UNICHAIN_SEPOLIA!,
         VITE_SUPERCHAIN_BADGES_ADDRESS: process.env.SUPERCHAIN_BADGES_ADDRESS!,
         VITE_SUPERCHAIN_POINTS_ADDRESS: process.env.SUPERCHAIN_POINTS_ADDRESS!,
-        VITE_SUPERCHAIN_POINTS_RAFFLE_FACTORY_ADDRESS:
-          process.env.SUPERCHAIN_POINTS_RAFFLE_FACTORY_ADDRESS!,
+        VITE_SUPERCHAIN_POINTS_RAFFLE_FACTORY_ADDRESS: process.env.SUPERCHAIN_POINTS_RAFFLE_FACTORY_ADDRESS!,
       },
     });
 
