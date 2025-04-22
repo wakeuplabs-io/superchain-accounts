438a1a16e08933b10564aba1813f6e15d4200eca
Matias Barro <matias.barro@gmail.com>
2025-04-22 10:21:51 -0300

    Close wallet connect sessions on logout

--- packages/ui/src/components/sidebar/logout-button.tsx
+++ packages/ui/src/components/sidebar/logout-button.tsx
@@ -3,12 +3,15 @@ import { useRouter } from "@tanstack/react-router";
 import { useAuth } from "@/hooks/use-auth";
 import { LogOut } from "lucide-react";
 import { ConfirmDialog } from "../confirm-dialog";
+import { useWalletConnect } from "@/hooks/use-wallet-connect";
 
 export function LogoutButton() {
   const router = useRouter();
   const { logout } = useAuth();
+  const { handleDisconnect } = useWalletConnect();
 
   const onLogout = async () => {
+    await handleDisconnect();
     await logout();
     router.history.push("/login");
   };
