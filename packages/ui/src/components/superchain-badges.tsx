77ab754bd3a28a3ba3c4e73c8595f3bed5566b3b
david fraga <david.fraga610@hotmail.com>
2025-04-30 15:05:14 -0600

    fix: use DialogTrigger

--- packages/ui/src/components/superchain-badges.tsx
+++ packages/ui/src/components/superchain-badges.tsx
@@ -8,29 +8,11 @@ import { ArrowRight, ArrowUpRight } from "lucide-react";
 import emptySvg from "@/assets/empty.svg";
 import { cn } from "@/lib/utils";
 import { Dialog, DialogContent, DialogTitle } from "@radix-ui/react-dialog";
-import { DialogHeader } from "./ui/dialog";
+import { DialogHeader, DialogTrigger } from "./ui/dialog";
 
 export const SuperchainBadges: React.FC = () => {
   const { isPending, claim, isClaiming, badges } =
     useSuperchainBadges();
-
-  const onClickNftUrl = useCallback(
-    (url: string, tokenName: string) => {
-      <Dialog>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>{tokenName}</DialogTitle>
-          </DialogHeader>
-          <div className="flex flex-col w-full items-center gap-12 justify-center pt-16 pb-8">
-            <div className="relative">
-                <img src={url}/>
-            </div>
-          </div>
-        </DialogContent>
-      </Dialog>
-    },
-    []
-  );
 
   const onClaim = useCallback(
     async (tokenId: bigint) => {
@@ -126,16 +108,31 @@ export const SuperchainBadges: React.FC = () => {
                 <div className="space-y-1">
                   <div className="font-semibold text-sm">{badge.name}</div>
                   <div className="text-xs">{badge.description}</div>
-                  {badge.status === "claimed" && <div className="w-fit text-sm text-green-600 bg-green-100 border border-green-600 px-2 rounded">Claimed</div>}
                   {badge.status === "claimed" && 
-                    (
-                      <a
-                        onClick={() => onClickNftUrl(badge.nftUrl, badge.name)}
-                        className="flex items-center gap-1 text-sm font-medium text-primary"
-                      >
-                        <span>View my NFT</span>
-                        <ArrowUpRight className="h-4 w-4" />
-                      </a>
+                    (<>
+                        <div className="w-fit text-sm text-green-600 bg-green-100 border border-green-600 px-2 rounded">Claimed</div>
+                        <Dialog>
+                            <DialogTrigger>
+                              <Button 
+                                className="flex items-center gap-1 text-sm font-medium text-primary"
+                                variant="link"
+                                >
+                                <span>View my NFT</span>
+                                <ArrowUpRight className="h-4 w-4" />
+                              </Button>
+                            </DialogTrigger>
+                          <DialogContent>
+                            <DialogHeader>
+                              <DialogTitle>{badge.name}</DialogTitle>
+                            </DialogHeader>
+                            <div className="flex flex-col w-full items-center gap-12 justify-center pt-16 pb-8">
+                              <div className="relative">
+                                  <img src={badge.nftUrl}/>
+                              </div>
+                            </div>
+                          </DialogContent>
+                        </Dialog>
+                      </>
                     )
                   }
                   {badge.status === "unclaimed" && (
