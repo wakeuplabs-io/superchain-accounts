374bfb0911e4af512c2a434e32714fd52d7bcb0d
Luciano Pérez Cerra <perezcerraluciano@gmail.com>
2025-04-30 15:40:12 -0300

    fix wallet connect metadata url

--- packages/ui/src/config/wallet-connect.ts
+++ packages/ui/src/config/wallet-connect.ts
@@ -12,6 +12,6 @@ export const SUPPORTED_EVENTS = ["accountsChanged", "chainChanged"];
 export const WALLET_METADATA = {
   name: "Superchain Wallet",
   description: "Superchain Wallet",
-  url: "https://superchain.wakeuplabs.link",
-  icons: ["https://superchain.wakeuplabs.link/favicon.ico"],
+  url: "https://superchain-accounts-ui.vercel.app",
+  icons: ["https://superchain-accounts-ui.vercel.app/favicon.ico"],
 };
